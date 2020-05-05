d222f3e666f7a7ea4eb8f805b9e1333bfb591bd8
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2020-05-05 15:03:27 +0700

    Add Some Components, Pipe, Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CoreModule } from "./core/core.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UserComponent } from "./user/user.component";
+import { TransformDirective } from "./transform.directive";
+import { SafePipe } from "./pipes/safe/safe.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, UserComponent, TransformDirective, SafePipe],
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
